8b32c029ae33b0508883a477f2046bf0ac22babe
Philip Bell <philip.hunter.bell@gmail.com>
2017-03-17 10:12:41 -0400

    build out digital wireframes. begin building appendix view

--- clientView/testIndex.js
+++ clientView/testIndex.js
@@ -16,6 +16,8 @@ function generate(value) {
   // const text = 'lab'
   const text = value;
 
+  let definitionData;
+
   // e.preventDefault();
 
   request.get(`/api${text}`)
@@ -35,14 +37,17 @@ function generate(value) {
           //  let colorData = data.body.returnItem.colorData
           //  console.log(colorData);
 
-           console.log(data.body.returnItem);
+          //  console.log(data.body.returnItem);
           //  console.log(data.body.returnItem);
           //  console.log(data.body.returnItem.dictData.results);
+           definitionData = data.body.returnItem.dictData.results;
+           handleDicta(definitionData)
 
            let iconData = data.body.returnItem.iconData
+
            iconData.forEach(icon => {
              let imageURL = icon.preview_url;
-             console.log(icon)
+            //  console.log(icon)
              let animContainerL = document.createElement('div');
              let animContainerR = document.createElement('div');
              animContainerL.classList.add('animContainerL');
@@ -60,6 +65,17 @@ function generate(value) {
            })
 
          })
+}
+
+function handleDicta(definitionData) {
+  let dictObject = {}
+  console.log(definitionData);
+  // dictObject.word = definitionData.word;
+  // dictObject.language = definitionData.lexicalEntries[0]language;
+  // dictObject.lexicalCategory = definitionData.lexicalEntries[0]lexicalCategory;
+  // dictObject.phoneticSpelling = definitionData.lexicalEntries[0]phoneticSpelling;
+  // dictObject.entry = definitionData.lexicalEntries[0]phoneticSpelling;
+
 }
 
 
@@ -97,13 +113,13 @@ function animateEl(animContainerL, animContainerR) {
   let startX = random(window.innerWidth / 2, window.innerWidth / 4);
   let endY = random(0, window.innerHeight);
   let endX = window.innerWidth / 2;
-  let rotation = random(0, 360);
+  let rotation = random(0, 30);
   let delay = 0;
   let scalePure = (endY / window.innerHeight);
   let scale = random(0.1, .45);
   // let scale = random(0.1, 0.5) * scaleModifier;
 
-  animateEl.fromTo([animContainerL, animContainerR], .5, {
+  animateEl.fromTo([animContainerL, animContainerR], 1, {
     y: startY,
     x: startX,
     rotation: 0,
@@ -136,39 +152,76 @@ function random(min, max) {
 
 /////////// search window ///////////
 
+// console.log(closeSearch);
 const btn = document.querySelector('.btn');
-const overlay = document.querySelector('.overlay');
-const close = document.querySelector('#close');
-const overlayInput = document.querySelector('.overlay input');
-
+const searchOverlay = document.querySelector('.searchOverlay');
+const closeSearch = document.querySelector('#closeSearch');
+const overlayInput = document.querySelector('.searchOverlay input');
 btn.addEventListener('click', function(e) { toggleOverlay() })
-close.addEventListener('click', function(e) { closeOverlay() })
+closeSearch.addEventListener('click', function(e) { closeOverlay() })
 window.addEventListener('keydown', handleKeydown);
 
 function handleKeydown(e) {
   if (e.keyCode === 27) {
     closeOverlay();
   } else if (e.keyCode === 13) {
-    // console.log(overlayInput.value);
-    generate(overlayInput.value)
+    handleChange(overlayInput.value)
     closeOverlay();
   } else if (e.keyCode) {
     console.log(overlayInput.value);
-    if (overlay.classList.contains('fadeIn')) return ;
+    if (searchOverlay.classList.contains('fadeIn')) return ;
     overlayInput.value = '';
     toggleOverlay()
     overlayInput.focus()
   }
 }
 
+function handleChange(changeValue) {
+
+  let currentAnims = document.querySelectorAll('.compContainer > div');
+  TweenMax.to(currentAnims, 1, {scale:0, ease:Sine.easeInOut, onComplete:handleRemove, onCompleteParams:[changeValue]})
+
+  function handleRemove(changeValue) {
+    currentAnims = document.querySelectorAll('.compContainer > div');
+    currentAnims.forEach(currentAnim => {
+      currentAnim.remove();
+    })
+    TweenMax.killAll();
+  }
+  setTimeout( function() {
+    generate(overlayInput.value)
+  }, 1000 );
+
+}
+
+
 function toggleOverlay() {
-  if (!overlay.classList.contains('fadeIn')) overlayInput.value = ''; ;
-  overlay.classList.toggle('fadeIn')
+  if (!searchOverlay.classList.contains('fadeIn')) overlayInput.value = ''; ;
+  searchOverlay.classList.toggle('fadeIn')
   overlayInput.focus()
 }
 
 function closeOverlay() {
-  if(overlay.classList.contains('fadeIn')) {
-    overlay.classList.remove('fadeIn')
+  if(searchOverlay.classList.contains('fadeIn')) {
+    searchOverlay.classList.remove('fadeIn')
+  }
+}
+
+
+/////////// info window ///////////
+
+const infoOverlay = document.querySelector('.infoOverlay');
+const info = document.querySelector('.info');
+const closeInfoOverlay = document.querySelector('.closeInfoOverlay');
+info.addEventListener('click', function(e) { toggleInfoOverlay() })
+closeInfoOverlay.addEventListener('click', function(e) { fadeoutInfoOverlay() })
+
+function toggleInfoOverlay() {
+  infoOverlay.classList.toggle('fadeIn');
+}
+
+function fadeoutInfoOverlay() {
+  if( infoOverlay.classList.contains('fadeIn') ) {
+    infoOverlay.classList.remove('fadeIn')
   }
 }
