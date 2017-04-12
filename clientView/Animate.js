330807a8e4be3ed62c9720dc1a0e0958c6c20c79
Philip Bell <philip.hunter.bell@gmail.com>
2017-04-12 00:02:43 -0400

    integrate idle animation

--- clientView/Animate.js
+++ clientView/Animate.js
@@ -9,10 +9,25 @@ import ColorPropsPlugin from '../node_modules/gsap/ColorPropsPlugin.js';
 import getRandomVal from './getRandomVal.js';
 import { allSets } from './generateAnimDomElements.js'
 
+let width = window.innerWidth;
+let height = window.innerHeight;
+let centerX = width * .5;
+let centerY = height * .5;
+let maxRadius = -.4;
+let maxDistance = height * maxRadius;
+let tick = 0;
+
+window.addEventListener('resize', function(e) {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  centerX = width * .5;
+  centerY = height * .5;
+  maxDistance = height * maxRadius;
+})
+
 class Animate {
 
   static setValues(animL, animR) {
-
     TweenMax.set(animL, {
       pixi: {
         anchor: 0.5,
@@ -41,31 +56,16 @@ class Animate {
 
   static randomLocaiton(elements, options, resolve) {
 
-    // let xMin = window.innerWidth/4;
-    // let xMax = window.innerWidth/2 + 50;
-    //
-    // let yMin = window.innerHeight/5.5 + 50;
-    // let yMax = window.innerHeight/1.50 + 50;
-
     elements.forEach((element, i) => {
       let animL = element[0]
       let animR = element[1]
 
-      // let endX = randomInt(xMin, xMax);
-      // let endY = randomInt(yMin, yMax);
-      // let scale = ((endX - xMin)) / (xMax - xMin)
-
-      let width = window.innerWidth;
-      let height = window.innerHeight;
-
-      let centerX = width * .5;
-      let centerY = height * .5;
-      let maxRadius = -.4;
+      // let staticRadius = height * maxRadius;
       let radius = height * randomInt(0, maxRadius);
-      let maxDistance = height * maxRadius;
-
       let angle = randomInt(0,3.14);
 
+      // let startX = centerX + Math.sin(angle) * staticRadius;
+      // let startY = centerY + Math.cos(angle) * staticRadius;
       let startX = centerX + Math.sin(angle) * radius;
       let startY = centerY + Math.cos(angle) * radius;
 
@@ -173,6 +173,7 @@ class Animate {
   }
 
   static update() {
+    tick++
     let newx = Math.floor(testDivcords.x);
     let newy = Math.floor(testDivcords.y);
     let diffx = newx - oldx;
@@ -187,27 +188,13 @@ class Animate {
 
   static updatePos(animSet, i, diffy, diffx) {
 
-    // let xMin = window.innerWidth/4;
-    // let xMax = window.innerWidth/2 + 50;
-    //
-    // let yMin = window.innerHeight/5.5 + 50;
-    // let yMax = window.innerHeight/1.50 + 50;
-    //
-    // let scale = ((animL.x - xMin)) / (xMax - xMin);
+    // console.log(Math.sin(tick))
 
-    if(i%2 === 0) {diffy = diffy/2; diffx = diffx/2}
-    else if(i%3 === 0) {diffy = diffy/3; diffx = diffx/3}
+    if(i%2 === 0) {diffy = diffy/2; diffx = diffx/2;}
+    else if(i%3 === 0) {diffy = diffy/3; diffx = diffx/3;}
 
     let animL = animSet[0];
     let animR = animSet[1];
-
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-
-    let centerX = width * .5;
-    let centerY = height * .5;
-    let maxRadius = -.4;
-    let maxDistance = height * maxRadius;
 
     let xDistance = animL.x - centerX;
     let yDistance = animL.y - centerY;
@@ -236,36 +223,28 @@ class Animate {
   static checkPos(animSet, diffy, diffx) {
     let animL = animSet[0];
 
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-
-    let centerX = width * .5;
-    let centerY = height * .5;
-    let maxRadius = -.4;
-    let maxDistance = height * maxRadius;
-
     let xDistance = animL.x - centerX;
     let yDistance = animL.y - centerY;
 
-    let currentMaxX = Math.sqrt(maxDistance * maxDistance - yDistance * yDistance)
-    let currentMaxY =  Math.sqrt(maxDistance * maxDistance - xDistance * xDistance)
+    // let currentMaxX = Math.sqrt(maxDistance * maxDistance - yDistance * yDistance)
+    // let currentMaxY =  Math.sqrt(maxDistance * maxDistance - xDistance * xDistance)
 
     let distance = Math.sqrt(xDistance * xDistance + yDistance * yDistance);
-    let distancePercent = 1 - distance / Math.abs(maxDistance);
+    // let distancePercent = 1 - distance / Math.abs(maxDistance);
 
-    if (Math.abs(yDistance) > Math.abs(currentMaxY)-10) { TweenLite.set(animL, { pixi: { y: centerY + yDistance * -1 }})}
-    if (Math.abs(xDistance) > Math.abs(currentMaxX)) { TweenLite.set(animL, { pixi: { x: centerX + animL.width/2 - 1 }})}
-    if ((animL.x - animL.width/2) > centerX) { TweenLite.set(animL, { pixi: { x: centerX - currentMaxX }})}
+    // Math.sin(tick)
 
-    // let xMin = window.innerWidth/4;
-    // let xMax = window.innerWidth/2 + 50;
-    // let yMin = window.innerHeight/5.5 + 50;
-    // let yMax = window.innerHeight/1.50 + 50;
+    let staticRadius = height * maxRadius;
+    let angle = Math.abs(Math.sin(tick/25)*randomInt(0,1)) * 3.14;
+    // let angle = randomInt(0,3.14);
 
-    // if (animL.x < xMin) { TweenLite.set(animL, { pixi: { x: xMax + 50 }})}
-    // if (animL.x > xMax + 50) { TweenLite.set(animL, { pixi: { x: xMin }})}
-    // if (animL.y < yMin - animL.height/2) { TweenLite.set(animL, { pixi: { y: yMax + animL.height/2 }})}
-    // if (animL.y > yMax + animL.height/2) { TweenLite.set(animL, { pixi: { y: yMin - animL.height/2 }})}
+    let startX = centerX + Math.sin(angle) * staticRadius;
+    let startY = centerY + Math.cos(angle) * staticRadius;
+    let randomY = centerY + randomInt(-maxDistance, maxDistance);
+
+    if ((animL.x - 100) > centerX) { TweenLite.set(animL, { pixi: { x: startX, y: startY }})}
+    if (Math.abs(distance) > Math.abs(maxDistance)) { TweenLite.set(animL, { pixi: {x: centerX + 100, y: randomY }})}
+
   }
 
 }
@@ -273,6 +252,9 @@ class Animate {
 module.exports = Animate;
 
 ///////////// draggable ////////////
+
+let oldx = 0;
+let oldy = 0;
 
 let body = document.body;
 let dragWrap = document.querySelector('.dragWrap');
@@ -286,27 +268,38 @@ Draggable.create(testDiv, {
   onThrowComplete: Animate.update,
   onThrowUpdate: Animate.update,
   onDrag: Animate.update,
-  trigger: body,
+  onDragStart: ()=> mouseIsDown = true,
+  onDragEnd: ()=> mouseIsDown = false,
+  trigger: dragWrap,
 });
 
 let testDivcords = Draggable.get(testDiv);
 
-let oldx = 0;
-let oldy = 0;
+///////////// idle animation ////////////
+
+let mouseDistanceX = 0;
+let mouseDistanceY = 0;
+
+window.addEventListener('mousemove', function(e) {
+  mouseDistanceX = ((e.clientX - window.innerWidth/2) / window.innerWidth*2)*2;
+  mouseDistanceY = ((e.clientY - window.innerHeight/2) / window.innerHeight*2*2);
+})
+
+let mouseIsDown = false
+
+TweenLite.ticker.addEventListener("tick", mouseAnim);
+
+function mouseAnim() {
+  if(mouseIsDown) return
+  allSets.forEach((animSet, i)=>{
+    Animate.checkPos(animSet);
+    Animate.updatePos(animSet, i, mouseDistanceY, mouseDistanceX);
+  })
+
+}
 
 ///////////// random int ////////////
 
 function randomInt(min, max) {
   return Math.random() * (max - min) + min;
 }
-
-///////////// idle animation ////////////
-
-// TweenLite.ticker.addEventListener("tick", yourFunction);
-//
-// function yourFunction() {
-//   allSets.forEach((animSet, i)=>{
-//     checkPos(animSet);
-//     updatePos(animSet, i, 0, .5);
-//   })
-// }
